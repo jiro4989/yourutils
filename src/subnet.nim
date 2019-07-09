f0b0274be2a99cbcfcb598596371120c20c953ba
jiro4989 <jiroron666@gmail.com>
2019-07-09 18:14:29 +0900

    :fire: 不要なコードを削除。色を付けるようにした

--- src/subnet.nim
+++ src/subnet.nim
@@ -1,45 +1,11 @@
-import clitools/[log, option]
-import strutils, strformat, terminal, logging, net, parseopt
-from os import commandLineParams
+import strutils, net
 
 type
-  Options* = ref object of RootOptions
-    printIPAddress*, printCIDR*, printIPAddressBin*, printIPAddressBinMask*, printColoredIPAddress*, printHeader*: bool
-    delimiter*: string
   IPv4* = object
     address*: string
-    CIDR*: int
+    cidr*: int
     bin*: string
-    parseMask*: string
     mask*: string
-
-const
-  appName = "subnet"
-  version = "v1.0.0"
-  doc = &"""
-{appName} is subnet calc.
-
-Usage:
-    {appName} [options] <ip/cidr>...
-    {appName} [options]
-
-Options:
-    -a, --ipaddress          show IP address
-    -r, --cidr               show CIDR
-    -b, --bin                show IP binaries
-    -m, --mask               show IP binaries Mask
-    -c, --color              show colored IP binaries
-    -d, --delimiter=<d>      output delimiter [default: \t]
-    -H, --header             show header column
-    -X, --debug              set on loggin level 'debug'
-
-help options:
-  -h --help         show this screen
-  -v --version      show version
-"""
-
-var
-  useDebug: bool
 
 proc parseIPNumber*(ip: string): seq[int] =
   ## 0-255
@@ -78,176 +44,76 @@ proc toMask*(i: int): string =
   let zero = '0'.repeat(32 - n)
   result = $one & $zero
 
-proc parseCIDR*(ipCIDR: string): IPv4 = 
+proc parseCidr*(ipCidr: string): IPv4 = 
   ## parseCIDRはIP/CIDR文字列をパースしてIPv4オブジェクトとして返す
-  let
-    ipc = ipCIDR.split "/"
-    ip = ipc[0]
-  var
-    cidr = ipc[1].parseInt
+  let ipc = ipCidr.split "/"
+  let ip = ipc[0]
+  var cidr = ipc[1].parseInt
   if cidr < 1:
     cidr = 1
   elif 32 < cidr:
     cidr = 32
-  let
-    bin = ip.toIPv4bin
-    mask = cidr.toMask
-  result = IPv4(address: ip, CIDR: cidr, bin: bin, mask: mask)
+  let bin = ip.toIPv4bin
+  let mask = cidr.toMask
+  result = IPv4(address: ip, cidr: cidr, bin: bin, mask: mask)
 
-proc subnet(showIpAddress: var bool = false, showCidr: var bool = false,
-            showIpBin: var bool = false, showIpBinMask: var bool = false,
-            useColor=false, delimiter="\t", showHeader=false,
+proc format(ipv4: IPv4, showIpAddress: bool, showCidr: bool,
+            showIpBin: bool, showIpBinMask: bool,
+            useColor: bool, delimiter: string): string =
+  var col: seq[string]
+  if showIpAddress: col.add(ipv4.address)
+  if showCidr: col.add($ipv4.cidr)
+  if showIpBin:
+    if useColor:
+      let bin = ipv4.bin
+      let cidr = ipv4.cidr
+      col.add("\e[31m" & bin[0..<cidr] & "\e[32m" & bin[cidr..^1] & "\e[m")
+    else:
+      col.add(ipv4.bin)
+  if showIpBinMask: col.add(ipv4.mask)
+  result = col.join(delimiter)
+
+proc subnet(showIpAddress = true, showCidr = true,
+            showIpBin = true, showIpBinMask = true,
+            useColor=false, delimiter="\t", showHeader=true,
             args: seq[string]): int =
-  # オプションがすべてfalseなら全部trueにする
-  # wcコマンドと同じような設定のしかた
-  if not showIPAddress and not showCidr and not showIPBin and not showIPBinMask:
-    showIPAddress = true
-    showCIDR = true
-    showIPBin = true
-    showIPBinMask = true
+  # # オプションがすべてfalseなら全部trueにする
+  # # wcコマンドと同じような設定のしかた
+  # if not showIPAddress and not showCidr and not showIPBin and not showIPBinMask:
+  #   showIPAddress = true
+  #   showCIDR = true
+  #   showIPBin = true
+  #   showIPBinMask = true
+  if showHeader:
+    var header: seq[string]
+    if showIpAddress: header.add("ip_address")
+    if showCidr: header.add("cidr")
+    if showIpBin: header.add("bin")
+    if showIpBinMask: header.add("mask")
+    echo header.join(delimiter)
 
   # 引数（ファイル）の指定がなければ標準入力を処理対象にする
   if args.len < 1:
-    debug "read stdin"
     for line in stdin.lines:
-      #printCodepoint line
-      discard
+      let ipv4 = line.parseCidr
+      echo ipv4.format(showIpAddress=showIpAddress,
+                       showCidr=showCidr,
+                       showIpBin=showIpBin,
+                       showIpBinMask=showIpBinMask,
+                       useColor=useColor,
+                       delimiter=delimiter)
     return
 
-  # 引数があればファイルの中身を読み取って処理する
-  debug "read args files"
+  # 引数があればそれを入力として扱う
   for arg in args:
-    for line in arg.lines:
-      #printCodepoint line
-      discard
+    let ipv4 = arg.parseCidr
+    echo ipv4.format(showIpAddress=showIpAddress,
+                     showCidr=showCidr,
+                     showIpBin=showIpBin,
+                     showIpBinMask=showIpBinMask,
+                     useColor=useColor,
+                     delimiter=delimiter)
 
 when isMainModule:
   import cligen
   dispatch(subnet)
-
-when false:
-
-
-  let args = docopt(doc, version="v1.0.0")
-
-  proc echoHeader(ipAddressFlag: bool = false,
-                  cidrFlag: bool = false,
-                  binFlag: bool = false,
-                  maskFlag: bool = false,
-                  delimiter: string = "\t"
-                  ) =
-    ## echoHeader はヘッダを出力する
-    var s: string
-    if ipAddressFlag:
-      s.add("IPAddr" & delimiter)
-    if cidrFlag:
-      s.add("CIDR" & delimiter)
-    if binFlag:
-      s.add("Bin" & delimiter)
-    if maskFlag:
-      s.add("Mask" & delimiter)
-    echo s
-
-  proc echoValue(ipv4: IPv4,
-                    ipAddressFlag: bool = false,
-                    cidrFlag: bool = false,
-                    binFlag: bool = false,
-                    maskFlag: bool = false,
-                    colorFlag: bool = false,
-                    delimiter: string = "\t"
-                    ) =
-    ## echoValue はオプションに応じてIPv4の出力内容を変更して標準出力する
-    if ipAddressFlag:
-      stdout.write(ipv4.address, delimiter)
-    if cidrFlag:
-      stdout.write(ipv4.CIDR, delimiter)
-    if binFlag:
-      if colorFlag:
-        let
-          cidr = ipv4.CIDR
-          b = ipv4.bin
-          netdiv = b[0 ..< cidr]
-          hostdiv = b[cidr .. b.len - 1]
-        stdout.styledWrite(fgRed, netdiv)
-        stdout.styledWrite(fgGreen, hostdiv)
-        stdout.write(delimiter)
-      else:
-        stdout.write(ipv4.bin, delimiter)
-    if maskFlag:
-      stdout.write(ipv4.mask)
-    stdout.write("\n")
-
-  proc initLogger(debugFlag: bool) =
-    ## initLoggerはロガーを初期化する
-    ## オプション引数に指定があれば全ログレベルを出力するフラグをたてる
-    var lvl: logging.Level
-    if debugFlag:
-      lvl = lvlAll
-    else:
-      lvl = lvlInfo
-    var L = newConsoleLogger(lvl, fmtStr = "$datetime [$levelname]$appname:")
-    addHandler(L)
-
-  if isMainModule:
-    let ipcidrs = @(args["<ip/cidr>"])
-    var
-      ipAddressFlag = ($args["--ipaddress"]).parseBool
-      cidrFlag = ($args["--cidr"]).parseBool
-      binFlag = ($args["--bin"]).parseBool
-      maskFlag = ($args["--mask"]).parseBool
-      colorFlag = ($args["--color"]).parseBool
-      outDelimiter = ($args["--delimiter"]).replace("\\t", "\t")
-      debugFlag = ($args["--debug"]).parseBool
-      headerFlag = ($args["--header"]).parseBool
-
-    initLogger debugFlag
-
-    debug "args:", args
-    
-    # オプションがすべてfalseなら全部trueにする
-    # wcコマンドと同じような設定のしかた
-    if not ipAddressFlag and not cidrFlag and not binFlag and not maskFlag:
-      ipAddressFlag = true
-      cidrFlag = true
-      binFlag = true
-      maskFlag = true
-    
-    if headerFlag:
-      echoHeader(ipAddressFlag = ipAddressFlag,
-                cidrFlag = cidrFlag,
-                binFlag = binFlag,
-                maskFlag = maskFlag,
-                delimiter = outDelimiter)
-
-    # 引数指定がなければ標準入力を処理
-    if ipcidrs.len < 1:
-      debug "process stdin"
-      var line: string
-      while stdin.readLine line:
-        try:
-          let ipv4 = line.parseCIDR
-          ipv4.echoValue(ipAddressFlag = ipAddressFlag,
-                            cidrFlag = cidrFlag,
-                            binFlag = binFlag,
-                            maskFlag = maskFlag,
-                            colorFlag = colorFlag,
-                            delimiter = outDelimiter)
-        except ValueError:
-          warn "invalid value:", line, " and skiped calc."
-      quit 0
-
-    # 引数指定がアレば引数を処理
-    debug "process args:", $ipcidrs
-    for ipcidr in ipcidrs:
-      try:
-        let ipv4 = ipcidr.parseCIDR
-        ipv4.echoValue(ipAddressFlag = ipAddressFlag,
-                          cidrFlag = cidrFlag,
-                          binFlag = binFlag,
-                          maskFlag = maskFlag,
-                          colorFlag = colorFlag,
-                          delimiter = outDelimiter)
-      except ValueError:
-        warn "invalid value:", ipcidr, " and skiped calc."
-
-
