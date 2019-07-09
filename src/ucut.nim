43742eb90016f60661026a4d56ec7487155e3ec8
jiro4989 <jiroron666@gmail.com>
2019-07-09 13:43:47 +0900

    :sparkles: cligenを使うように書き直した

--- src/ucut.nim
+++ src/ucut.nim
@@ -1,71 +1,29 @@
 import argparse
 
-import clitools/[io, log, option]
-import clitools/private/common
+import strutils, sequtils
 
-import alignment
-import parseopt, logging
-from strutils import parseInt, split, join
-from strformat import `&`
-from sequtils import mapIt, repeat
-from os import commandLineParams
-
-type
-  Options* = ref object of RootOptions
-    delimiter*: string
-    outputDelimiter*: string
-    fields*: seq[int]
-
-const
-  appName = "ucut"
-
-var
-  version: string
-  useDebug: bool
-
-include clitools/private/version
-
-proc cut*(line: string, opts: Options): string =
-  let fields = line.split(opts.delimiter)
+proc cut*(line: string, delimiter: string, outputDelimiter: string, fields: seq[int]): string =
+  let fs = line.split(delimiter)
   var outf: seq[string]
-  for i in opts.fields:
+  for i in fields:
     let n = i - 1
-    if n < 0 or fields.len <= n: continue
-    outf.add fields[n]
-  result = outf.join(opts.outputDelimiter)
+    if n < 0 or fs.len <= n: continue
+    outf.add fs[n]
+  result = outf.join(outputDelimiter)
 
-proc main*(params: seq[string]) =
-  var p = newParser(appName):
-    flag("-v", "--version", help="Print version")
-    flag("-X", "--debug", help="Debug")
-    option("-d", "--delimiter", help="Delimiter", default = " ")
-    option("-D", "--output-delimiter", help="Output delimiter", default = " ")
-    option("-f", "--fields", help="Output delimiter", default = "1")
-    arg("args", nargs = -1)
-
-  let opt = p.parse(params)
-  setOptions:
-    let opts = Options(
-      delimiter: opt.delimiter,
-      outputDelimiter: opt.outputDelimiter,
-      fields: opt.fields.split(",").mapIt(it.parseInt),
-      args: opt.args)
+proc ucut(delimiter=" ", outputDelimiter=" ", fields="1", files: seq[string]): int =
+  let fs = fields.split(",").mapIt(it.parseInt)
 
   # 引数（ファイル）の指定がなければ標準入力を処理対象にする
-  if opts.args.len < 1:
-    debug "read stdin"
+  if files.len < 1:
     for line in stdin.lines:
-      echo line.cut(opts)
+      echo line.cut(delimiter, outputDelimiter, fs)
 
   # 引数があればファイルの中身を読み取って処理する
-  debug "read args files"
-  for arg in opts.args:
+  for arg in files:
     for line in arg.lines:
-      echo line.cut(opts)
+      echo line.cut(delimiter, outputDelimiter, fs)
 
 when isMainModule:
-  try:
-    main(commandLineParams())
-  except:
-    stderr.writeLine(getCurrentExceptionMsg())
-    quit 1
+  import cligen
+  dispatch(ucut, short = {"outputDelimiter": 'D'})
