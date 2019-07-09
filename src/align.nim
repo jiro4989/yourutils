c67364b2b499d767921c889bac422faa7e0042b8
jiro4989 <jiroron666@gmail.com>
2019-07-09 14:57:23 +0900

    :recycle: cligenを使うようにしたけれどコンパイルできない

--- src/align.nim
+++ src/align.nim
@@ -1,124 +1,69 @@
-import clitools/[io, log]
 import alignment
-import parseopt, logging
-from strutils import parseInt, join
-from strformat import `&`
-from sequtils import mapIt, repeat
-from os import commandLineParams
 
-type
-  Options* = ref object
-    help*: bool
-    version*: bool
-    subcmd*: string
-    length*: int
-    pad*: string
-    writeFile*: bool
-    args*: seq[string]
+import clitools/io
 
-const
-  appName = "align"
-  version = "v1.0.0"
-  doc = &"""
-{appName} aligns texts.
+import strutils, sequtils
 
-Usage:
-    {appName} (-h | --help | -v | --version)
-    {appName} left   [options] [files...]
-    {appName} center [options] [files...]
-    {appName} right  [options] [files...]
-
-Options:
-    -h, --help                Print this help
-    -v, --version             Print version
-    -X, --debug               Print debug log
-    -n, --length:int          Padding length
-    -p, --pad:string          Padding string
-    -w, --writefile           Overwrite file
-"""
-
-var
-  useDebug: bool
-
-proc getCmdOpts*(params: seq[string]): Options =
-  var optParser = initOptParser(params)
-  new result
-  result.pad = " "
-
-  # コマンドラインオプションを取得
-  for kind, key, val in optParser.getopt():
-    case kind
-    of cmdArgument:
-      if result.subcmd == "":
-        case key
-        of "left", "center", "right":
-          result.subcmd = key
-      else:
-        result.args.add key
-    of cmdLongOption, cmdShortOption:
-      case key
-      of "help", "h":
-        echo doc
-        result.help = true
-        return
-      of "version", "v":
-        echo version
-        result.version = true
-        return
-      of "debug", "X":
-        useDebug = true
-      of "length", "n":
-        result.length = val.parseInt
-        doAssert 0 < result.length, &"{appName}: {key} = {result.length}: parameters is illegal"
-      of "pad", "p":
-        result.pad = val
-      of "writeFile", "w":
-        result.writeFile = true
-    of cmdEnd:
-      assert(false)  # cannot happen
-
-
-proc execSubcmd(f: File, lines: openArray[string], opts: Options) =
-  let
-    n = opts.length
-    p = opts.pad
-  var aligned: seq[string]
-  case opts.subcmd
-  of "left":
-    aligned = lines.alignLeft(pad = opts.pad).mapIt(it & p.repeat(n).join)
-  of "center":
-    var m = int(n / 2)
-    aligned = lines.alignCenter(pad = opts.pad).mapIt(p.repeat(m).join & it & p.repeat(m).join)
-  of "right":
-    aligned = lines.alignRight(pad = opts.pad).mapIt(p.repeat(n).join & it)
-  else: discard # 到達しない
-
-  for line in aligned:
-    f.writeLine line
-
-when isMainModule:
-  let opts = getCmdOpts(commandLineParams())
-  if opts.help or opts.version: quit 0
-
-  setDebugLogger useDebug
-  
-  debug "options = ", opts[]
-  
+proc left(length=0, pad=" ", writeFile=false, files: seq[string]): int =
   # 引数（ファイル）の指定がなければ標準入力を処理対象にする
-  if opts.args.len < 1:
-    debug "read stdin"
+  if files.len < 1:
     let lines = stdin.readLines
-    execSubcmd stdout, lines, opts
-    quit 0
+    for line in lines.alignLeft(pad = pad).mapIt(it & pad.repeat(length).join):
+      echo line
+    return
 
   # 引数があればファイルの中身を読み取って処理する
-  debug "read args files"
-  for arg in opts.args:
-    var f = open(arg)
+  for file in files:
+    var f = open(file)
     let lines = f.readLines
     f.close
 
-    var outf = if opts.writeFile: open(arg, fmWrite)
+    var outf = if writeFile: open(file, fmWrite)
                else: stdout
-    execSubcmd outf, lines, opts
-    outf.close
+    for line in lines.alignLeft(pad = pad).mapIt(it & pad.repeat(length).join):
+      outf.writeLine(line)
+    if outf != stdout: outf.close
+
+proc center(length=0, pad=" ", writeFile=false, files: seq[string]): int =
+  # 引数（ファイル）の指定がなければ標準入力を処理対象にする
+  if files.len < 1:
+    let lines = stdin.readLines
+    for line in lines.alignCenter(pad = pad).mapIt(it & pad.repeat(length).join):
+      echo line
+    return
+
+  # 引数があればファイルの中身を読み取って処理する
+  for file in files:
+    var f = open(file)
+    let lines = f.readLines
+    f.close
+
+    var outf = if writeFile: open(file, fmWrite)
+               else: stdout
+    for line in lines.alignCenter(pad = pad).mapIt(it & pad.repeat(length).join):
+      outf.writeLine(line)
+    if outf != stdout: outf.close
+
+proc right(length=0, pad=" ", writeFile=false, files: seq[string]): int =
+  # 引数（ファイル）の指定がなければ標準入力を処理対象にする
+  if files.len < 1:
+    let lines = stdin.readLines
+    for line in lines.alignRight(pad = pad).mapIt(it & pad.repeat(length).join):
+      echo line
+    return
+
+  # 引数があればファイルの中身を読み取って処理する
+  for file in files:
+    var f = open(file)
+    let lines = f.readLines
+    f.close
+
+    var outf = if writeFile: open(file, fmWrite)
+               else: stdout
+    for line in lines.alignRight(pad = pad).mapIt(it & pad.repeat(length).join):
+      outf.writeLine(line)
+    if outf != stdout: outf.close
+
+when isMainModule and false:
+  import cligen
+  dispatchMulti([left], [center])
