4bf359b13b4b8ad923fc4937c04169e6d0fa9f0b
jiro4989 <jiroron666@gmail.com>
2019-07-09 17:33:48 +0900

    :green_heart: テストコケていたのを修正

--- tests/tucut.nim
+++ tests/tucut.nim
@@ -3,8 +3,8 @@ import ucut
 
 suite "cut":
   test "1 field":
-    check "1,2,3".cut(Options(delimiter: ",", outputDelimiter: ",", fields: @[1])) == "1"
-    check "あ,い,う".cut(Options(delimiter: ",", outputDelimiter: ",", fields: @[2])) == "い"
+    check "1,2,3".cut(",", ",", @[1]) == "1"
+    check "あ,い,う".cut(",", ",", @[2]) == "い"
   test "2 field":
-    check "1,2,3".cut(Options(delimiter: ",", outputDelimiter: " ", fields: @[1, 3])) == "1 3"
-    check "あんいんう".cut(Options(delimiter: "ん", outputDelimiter: "ン", fields: @[2, 3])) == "いンう"
+    check "1,2,3".cut(",", " ", @[1, 3]) == "1 3"
+    check "あんいんう".cut("ん", "ン", @[2, 3]) == "いンう"
