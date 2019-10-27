050c5a55e361b4d3f02c527d50feb15999109d06
jiro4989 <jiroron666@gmail.com>
2019-10-27 20:37:12 +0900

    機能をサブコマンドに分けた #7

--- src/renames.nim
+++ src/renames.nim
@@ -3,110 +3,158 @@ from strformat import `&`
 
 const
   whiteSpaces = @[" ", "　", "\t"]
-  version = """renames version 1.2.0
+  version = """renames version 2.0.0
 Copyright (c) 2019 jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/clitools"""
 
-proc renames(dryRun = false, printRename = false, whiteSpace = false,
-             fromStrs: seq[string] = @[], toStr = "",
-             lower = false, upper = false, deleteStrs: seq[string] = @[],
-             filter = false,
-             dirs: seq[string]): int =
-  ## Rename files or directories.
-  ## 一番下の階層から再帰的にリネームしてまわる。
+var
+  changeFileCount: int  # 変更対象のファイル件数
+  fileCount: int        # 走査した全てのファイル件数
+  changedFileCount: int # 実際に変更したファイル件数
 
-  # whitespaceを使う指定があれば置換元の文字列をwhiteSpaceにする
-  var fromStrs2 = fromStrs
-  if whiteSpace:
-    fromStrs2 = whiteSpaces
+proc printResult() =
+  echo ""
+  echo &"{fileCount} files, {changeFileCount} changes files, {changedFileCount} files changed"
 
-  # fromStrsとtoStrは必須なのでチェック
-  if not lower and not upper and deleteStrs.len < 1 and (fromStrs2.len < 1 or toStr.len < 1):
-    stderr.writeLine "[ ERR ] must need fromStrs and toStr"
-    stderr.writeLine "[ ERR ] see help"
-    return 1
+template printMsg(kind: PathComponent, path, newPath: string, filter: bool) =
+  let kindCol =
+    if kind == pcFile: fgYellow
+    else: fgBlue
+  let kindStr =
+    if kind == pcFile: "[ File ]"
+    else: "[ Dir  ]"
 
-  # 変更対象のファイル件数
-  var changeFileCount: int
-  template printMsg(kind: PathComponent, path, newPath: string) =
-    let kindCol =
-      if kind == pcFile: fgYellow
-      else: fgBlue
-    let kindStr =
-      if kind == pcFile: "[ File ]"
-      else: "[ Dir  ]"
+  if path != newPath:
+    inc(changeFileCount)
+    styledEcho fgBlack, kindCol, kindStr, resetStyle, " ", path, " -> ", fgGreen, newPath, resetStyle
+  else:
+    if not filter:
+      styledEcho fgBlack, kindCol, kindStr, resetStyle, " ", "NO CHANGE ", path
 
-    if path != newPath:
-      inc(changeFileCount)
-      styledEcho fgBlack, kindCol, kindStr, resetStyle, " ", path, " -> ", fgGreen, newPath, resetStyle
+proc runMoveFile(kind: PathComponent, path, newPath: string,
+                 dryRun, filter, printRename: bool) =
+  inc(fileCount)
+  if dryRun:
+    printMsg(kind, path, newPath, filter)
+    return
+
+  if printRename:
+    printMsg(kind, path, newPath, filter)
+  if path != newPath:
+    inc(changedFileCount)
+    moveFile(path, newPath)
+
+proc rename(dir: string, newPathProc: proc (path: string): string,
+            dryRun, filter, printRename: bool) =
+  for kind, path in walkDir(dir):
+    let newPath = newPathProc(path)
+
+    case kind
+    of pcDir:
+      rename(path, newPathProc,
+             dryRun = dryRun, filter = filter, printRename = printRename)
+      runMoveFile(kind, path, newPath,
+                  dryRun = dryRun, filter = filter, printRename = printRename)
+    of pcFile:
+      runMoveFile(kind, path, newPath,
+                  dryRun = dryRun, filter = filter, printRename = printRename)
     else:
-      if not filter:
-        styledEcho fgBlack, kindCol, kindStr, resetStyle, " ", "NO CHANGE ", path
+      discard
 
-  var fileCount: int        # 走査した全てのファイル件数
-  var changedFileCount: int # 実際に変更したファイル件数
-  template runRename(kind: PathComponent, path, newPath: string) =
-    inc(fileCount)
-    if dryRun:
-      printMsg(kind, path, newPath)
-    else:
-      if printRename:
-        printMsg(kind, path, newPath)
-      if path != newPath:
-        inc(changedFileCount)
-        moveFile(path, newPath)
+template renameDirs(dirs: seq[string], f: proc (path: string): string,
+                    dryRun, printRename, filter: bool) =
+  for dir in dirs:
+    rename(dir, f, dryRun = dryRun, filter = filter, printRename = printRename)
+    runMoveFile(pcDir, dir, f(dir), dryRun = dryRun, filter = filter, printRename = printRename)
 
+proc cmdDelete(dryRun = false, printRename = false, filter = false,
+            fromStrs: seq[string] = whiteSpaces,
+            dirs: seq[string]): int =
+  proc getRemovedName(path: string): string =
+    let (dir, name, ext) = splitFile(path)
+    let base = name & ext
+    var newBase = base
+
+    for s in fromStrs:
+      newBase = newBase.replace(s, "")
+    result = dir / newBase
+
+  renameDirs(dirs, getRemovedName, dryRun = dryRun, filter = filter, printRename = printRename)
+  printResult()
+
+proc cmdReplace(dryRun = false, printRename = false, filter = false,
+            fromStrs: seq[string] = whiteSpaces, toStr = "",
+            dirs: seq[string]): int =
   proc getReplaceName(path: string): string =
     let (dir, name, ext) = splitFile(path)
     let base = name & ext
     var newBase = base
 
-    # 大文字小文字変換があればそれだけやる
-    if lower:
-      newBase = toLowerAscii(newBase)
-    elif upper:
-      newBase = toUpperAscii(newBase)
-    elif 1 <= deleteStrs.len:
-      # 削除文字があれば削除だけ
-      for s in deleteStrs:
-        newBase = newBase.replace(s, "")
-    else:
-      # どちらもなければ、置換文字を使う
-      for subs in fromStrs2:
-        newBase = newBase.replace(subs, toStr)
+    for s in fromStrs:
+      newBase = newBase.replace(s, toStr)
     result = dir / newBase
 
-  proc rename(dir: string) =
-    for kind, path in walkDir(dir):
-      let newPath = getReplaceName(path)
+  renameDirs(dirs, getReplaceName, dryRun = dryRun, filter = filter, printRename = printRename)
+  printResult()
 
-      case kind
-      of pcDir:
-        rename(path)
-        runRename(kind, path, newPath)
-      of pcFile:
-        runRename(kind, path, newPath)
-      else:
-        discard
+proc cmdLower(dryRun = false, printRename = false, filter = false,
+           dirs: seq[string]): int =
+  proc getLowerName(path: string): string =
+    let (dir, name, ext) = splitFile(path)
+    let base = name & ext
+    result = dir / toLowerAscii(base)
 
-  for dir in dirs:
-    rename(dir)
-    runRename(pcDir, dir, getReplaceName(dir))
+  renameDirs(dirs, getLowerName, dryRun = dryRun, filter = filter, printRename = printRename)
+  printResult()
 
-  echo ""
-  echo &"{fileCount} files, {changeFileCount} changes files, {changedFileCount} files changed"
+proc cmdUpper(dryRun = false, printRename = false, filter = false,
+           dirs: seq[string]): int =
+  proc getUpperName(path: string): string =
+    let (dir, name, ext) = splitFile(path)
+    let base = name & ext
+    result = dir / toUpperAscii(base)
+
+  renameDirs(dirs, getUpperName, dryRun = dryRun, filter = filter, printRename = printRename)
+  printResult()
 
 when isMainModule:
   import cligen
   clCfg.version = version
-  dispatch(renames,
-           short = {"deleteStrs":'D', "filter":'F'},
-           help = {
-             "whiteSpace":"replace name from white spaces to `toStr`",
-             "fromStrs":"replace name from `fromStrs` to `toStr`",
-             "toStr":"replace name from `fromStrs` to `toStr`",
-             "printRename":"print rename action when this command renames files",
-             "dryRun":"NO rename, but print rename action. You can check rename",
-             "filter":"filtering no change files",
-             })
+
+  const
+    helpPrintRename = "print rename action when this command renames files"
+    helpDryRun = "NO rename, but print rename action. You can check rename"
+    helpFilter = "filtering no change files"
+  dispatchMulti([cmdDelete, cmdName = "delete",
+                 short = {"filter":'F'},
+                 help = {
+                   "fromStrs":"delete characters",
+                   "printRename":helpPrintRename,
+                   "dryRun":helpDryRun,
+                   "filter":helpFilter,
+                   }],
+                [cmdReplace, cmdName = "replace",
+                 short = {"filter":'F'},
+                 help = {
+                   "fromStrs":"replace name from white spaces to `toStr`",
+                   "toStr":"replace name from `fromStrs` to `toStr`",
+                   "printRename":helpPrintRename,
+                   "dryRun":helpDryRun,
+                   "filter":helpFilter,
+                   }],
+                [cmdLower, cmdName = "lower",
+                 short = {"filter":'F'},
+                 help = {
+                   "printRename":helpPrintRename,
+                   "dryRun":helpDryRun,
+                   "filter":helpFilter,
+                   }],
+                [cmdUpper, cmdName = "upper",
+                 short = {"filter":'F'},
+                 help = {
+                   "printRename":helpPrintRename,
+                   "dryRun":helpDryRun,
+                   "filter":helpFilter,
+                   }],
+                )
